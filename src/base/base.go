e6b6c2030c7a3ad9838b0c002b85704a82151c55
hellboy1975 <hellboy1975@gmail.com>
2023-12-02 19:36:50 +1030

    Add readLines function

--- src/base/base.go
+++ src/base/base.go
@@ -1,7 +1,11 @@
 package base
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
 const testDataFile = "day_%d_%d%s.txt"
@@ -17,4 +21,34 @@ func GetTestDataFile(test bool, day, part int) string {
 	fmt.Println("  Test data file: " + testDataFile)
 
 	return testDataFile
+}
+
+// Read a whole file into the memory and store it as array of lines
+func readLines(path string) (lines []string, err error) {
+	var (
+		file   *os.File
+		part   []byte
+		prefix bool
+	)
+	if file, err = os.Open(path); err != nil {
+		return
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	buffer := bytes.NewBuffer(make([]byte, 0))
+	for {
+		if part, prefix, err = reader.ReadLine(); err != nil {
+			break
+		}
+		buffer.Write(part)
+		if !prefix {
+			lines = append(lines, buffer.String())
+			buffer.Reset()
+		}
+	}
+	if err == io.EOF {
+		err = nil
+	}
+	return
 }
