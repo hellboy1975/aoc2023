9db85a4c83f8bcaa6ec516720c9a6d0d0f1c40a3
matthew.smith <matthew.smith@aligent.com.au>
2023-12-05 15:22:39 +1030

    Add Day 2 Part 1 solution

--- src/day2/day_2.go
+++ src/day2/day_2.go
@@ -14,13 +14,47 @@ const max_blue = 14
 func GetGameId(s string) int {
 
 	id := strings.Split(s, ":")[0]
-	fmt.Println(id)
+	// fmt.Println(id)
 
 	id = strings.Split(id, " ")[1]
-	fmt.Println(id)
+	// fmt.Println(id)
 
 	intId, _ := strconv.Atoi(id)
 	return intId
+}
+
+func IsGamePossible(s string) bool {
+	colours := strings.Split(s, ":")[1]
+	rounds := strings.Split(colours, ";")
+
+	for _, r := range rounds {
+		sets := strings.Split(r, ",")
+		for _, s := range sets {
+			s = strings.TrimSpace(s)
+			// fmt.Println("set:", s)
+			colour := strings.Split(s, " ")[1]
+			count, _ := strconv.Atoi(strings.Split(s, " ")[0])
+
+			// fmt.Println(i, "colour", colour)
+			// fmt.Println(i, "count", count)
+
+			if colour == "blue" {
+				if count > max_blue {
+					return false
+				}
+			} else if colour == "red" {
+				if count > max_red {
+					return false
+				}
+			} else if colour == "green" {
+				if count > max_green {
+					return false
+				}
+			}
+		}
+	}
+	return true
+
 }
 
 // takes some elvish raw data, and pulls the ID out of it.
@@ -32,14 +66,14 @@ func GetColourCount(s string) (blue int, red int, green int) {
 
 	for _, r := range rounds {
 		sets := strings.Split(r, ",")
-		for i, s := range sets {
+		for _, s := range sets {
 			s = strings.TrimSpace(s)
-			fmt.Println("set:", s)
+			// fmt.Println("set:", s)
 			colour := strings.Split(s, " ")[1]
 			count, _ := strconv.Atoi(strings.Split(s, " ")[0])
 
-			fmt.Println(i, "colour", colour)
-			fmt.Println(i, "count", count)
+			// fmt.Println(i, "colour", colour)
+			// fmt.Println(i, "count", count)
 
 			if colour == "blue" {
 				blue += count
@@ -60,10 +94,23 @@ func Part1() {
 
 	file := base.GetDayDataFile(2, 1)
 
-	_, err := base.ReadLines(file)
+	lines, err := base.ReadLines(file)
 	if err != nil {
 		panic(err)
 	}
+
+	var sum int
+	for i, s := range lines {
+		id := GetGameId(s)
+		fmt.Println(i, ": ", s)
+
+		if IsGamePossible(s) {
+			fmt.Println("  Possible")
+			sum += id
+		}
+	}
+
+	fmt.Println("Sum of possible games:", sum)
 }
 
 func Part2() {
--- src/day2/day_2_test.go
+++ src/day2/day_2_test.go
@@ -44,3 +44,27 @@ func TestGetColourCount(t *testing.T) {
 	}
 
 }
+
+func TestIsGamePossible(t *testing.T) {
+
+	cases := []struct {
+		Description string
+		Raw         string
+		Want        bool
+	}{
+		{"Game 1 is possible", "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", true},
+		{"Game 2 is possible", "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", true},
+		{"Game 3 is impossible", "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", false},
+		{"Game 4 is impossible", "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", false},
+		{"Game 5 is possible", "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", true},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := IsGamePossible(test.Raw)
+			if got != test.Want {
+				t.Errorf("got %t, want %t", got, test.Want)
+			}
+		})
+	}
+}
