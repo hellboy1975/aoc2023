0cea7ef1ecff612247e90dcc98b32d1b86bf3cd5
matthew.smith <matthew.smith@aligent.com.au>
2023-12-06 14:43:09 +1030

    Tidy up the launcher

--- aoc2023.go
+++ aoc2023.go
@@ -15,17 +15,18 @@ func main() {
 
 	fmt.Println("Advent of Code 2023!")
 
+	//TODO: come up with a better method than a giant if statement
 	if *dayPtr == 1 {
 		if *partPtr == 1 {
 			day1.Part1()
 		} else {
-			day1.Part2() // pass an argument to reduce code reuse
+			day1.Part2()
 		}
 	} else if *dayPtr == 2 {
 		if *partPtr == 1 {
 			day2.Part1()
 		} else {
-			day2.Part2() // pass an argument to reduce code reuse
+			day2.Part2()
 		}
 	}
 }
