b3c06c1cb28276c176822829932cda4a125f0d01
hellboy1975 <hellboy1975@gmail.com>
2023-12-11 21:31:38 +1030

    Add some tests for the X and Y calc functions

--- src/day3/day_3_test.go
+++ src/day3/day_3_test.go
@@ -4,6 +4,130 @@ import (
 	"testing"
 )
 
-func TestDay3(t *testing.T) {
-	t.Errorf("No tests written!")
+func TestXcoord(t *testing.T) {
+	var want coord
+	want.x = 3
+	want.y = 2
+	chunk := []int{4, 6}
+	line := 3
+	got := Xcoord(chunk, line)
+
+	if got.x != want.x {
+		t.Errorf("X Coordinate got %d, want %d", got.x, want.x)
+	}
+	if got.y != want.y {
+		t.Errorf("Y Coordinate got %d, want %d", got.y, want.y)
+	}
+}
+
+func TestXcoordLineOne(t *testing.T) {
+	var want coord
+	want.x = 3
+	want.y = 1
+	chunk := []int{4, 6}
+	line := 1
+	got := Xcoord(chunk, line)
+
+	if got.x != want.x {
+		t.Errorf("X Coordinate got %d, want %d", got.x, want.x)
+	}
+	if got.y != want.y {
+		t.Errorf("Y Coordinate got %d, want %d", got.y, want.y)
+	}
+}
+
+func TestXcoordFinalLine(t *testing.T) {
+	var want coord
+	want.x = 3
+	want.y = 139
+	chunk := []int{4, 6}
+	line := DataExtent
+	got := Xcoord(chunk, line)
+
+	if got.x != want.x {
+		t.Errorf("X Coordinate got %d, want %d", got.x, want.x)
+	}
+	if got.y != want.y {
+		t.Errorf("Y Coordinate got %d, want %d", got.y, want.y)
+	}
+}
+
+func TestXcoordFirstCol(t *testing.T) {
+	var want coord
+	want.x = 0
+	want.y = 139
+	chunk := []int{0, 2}
+	line := DataExtent
+	got := Xcoord(chunk, line)
+
+	if got.x != want.x {
+		t.Errorf("X Coordinate got %d, want %d", got.x, want.x)
+	}
+	if got.y != want.y {
+		t.Errorf("Y Coordinate got %d, want %d", got.y, want.y)
+	}
+}
+
+func TestYcoord(t *testing.T) {
+	var want coord
+	want.x = 7
+	want.y = 4
+	chunk := []int{4, 6}
+	line := 3
+	got := Ycoord(chunk, line)
+
+	if got.x != want.x {
+		t.Errorf("X Coordinate got %d, want %d", got.x, want.x)
+	}
+	if got.y != want.y {
+		t.Errorf("Y Coordinate got %d, want %d", got.y, want.y)
+	}
+}
+
+func TestYcoordFirst(t *testing.T) {
+	var want coord
+	want.x = 3
+	want.y = 4
+	chunk := []int{0, 2}
+	line := 3
+	got := Ycoord(chunk, line)
+
+	if got.x != want.x {
+		t.Errorf("X Coordinate got %d, want %d", got.x, want.x)
+	}
+	if got.y != want.y {
+		t.Errorf("Y Coordinate got %d, want %d", got.y, want.y)
+	}
+}
+
+func TestYcoordLast(t *testing.T) {
+	var want coord
+	want.x = 139
+	want.y = 4
+	chunk := []int{137, 139}
+	line := 3
+	got := Ycoord(chunk, line)
+
+	if got.x != want.x {
+		t.Errorf("X Coordinate got %d, want %d", got.x, want.x)
+	}
+	if got.y != want.y {
+		t.Errorf("Y Coordinate got %d, want %d", got.y, want.y)
+	}
+}
+
+func TestYcoordLastCol(t *testing.T) {
+	var want coord
+	want.x = 139
+	want.y = 140
+	chunk := []int{137, 139}
+	line := DataExtent
+	got := Ycoord(chunk, line)
+
+	if got.x != want.x {
+		t.Errorf("X Coordinate got %d, want %d", got.x, want.x)
+	}
+	if got.y != want.y {
+		t.Errorf("Y Coordinate got %d, want %d", got.y, want.y)
+	}
 }
