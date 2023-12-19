0b7851a518cd67943c73fb948d9748920bf8102a
matthew.smith <matthew.smith@aligent.com.au>
2023-12-19 13:51:49 +1030

    Add RemoveDuplicateInt function to base package

--- src/base/base.go
+++ src/base/base.go
@@ -47,3 +47,16 @@ func ReadLines(path string) (lines []string, err error) {
 	}
 	return
 }
+
+// removes any duplicate integers from the passed slice
+func RemoveDuplicateInt(intSlice []int) []int {
+	allKeys := make(map[int]bool)
+	list := []int{}
+	for _, item := range intSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
