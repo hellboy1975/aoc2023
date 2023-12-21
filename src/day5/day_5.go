455daf6916a5237c291f08afb98b306a5371b0e3
hellboy1975 <hellboy1975@gmail.com>
2023-12-21 21:26:22 +1030

    Add ParseSeeds function and test

--- src/day5/day_5.go
+++ src/day5/day_5.go
@@ -3,18 +3,31 @@ package day5
 import (
 	"aoc2023/src/base"
 	"fmt"
+	"strings"
 )
 
 var file string
 
-func ParseSeeds() {
+func init() {
+	file = base.GetDayDataFile("5", "1")
+}
 
+// seeds are in the first line
+func ParseSeeds(line string) []int {
+
+	return base.StringToIntArray(strings.Split(line, ":")[1])
 }
 
 func Part1() {
 	fmt.Println("Day 5, Part 1: If You Give A Seed A Fertilizer")
 
-	file = base.GetDayDataFile("4", "1")
+	lines, err := base.ReadLines(file)
+	if err != nil {
+		panic(err)
+	}
+
+	seeds := ParseSeeds(lines[0])
+	fmt.Println("seeds:", seeds)
 }
 
 func Part2() {
--- src/day5/day_5_test.go
+++ src/day5/day_5_test.go
@@ -1,4 +1,32 @@
 package day5
 
+import (
+	"reflect"
+	"testing"
+)
+
 func init() {
+}
+
+func TestParseSeeds(t *testing.T) {
+
+	cases := []struct {
+		Description string
+		Raw         string
+		Want        []int
+	}{
+		{"Example seeds", "seeds: 79 14 55 13", []int{79, 14, 55, 13}},
+		{"My seeds",
+			"seeds: 3943078016 158366385 481035699 103909769 3553279107 15651230 3322093486 189601966 2957349913 359478652 924423181 691197498 2578953067 27362630 124747783 108079254 1992340665 437203822 2681092979 110901631",
+			[]int{3943078016, 158366385, 481035699, 103909769, 3553279107, 15651230, 3322093486, 189601966, 2957349913, 359478652, 924423181, 691197498, 2578953067, 27362630, 124747783, 108079254, 1992340665, 437203822, 2681092979, 110901631}},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ParseSeeds(test.Raw)
+			if reflect.DeepEqual(got, test.Want) != true {
+				t.Errorf("got %d, want %d", got, test.Want)
+			}
+		})
+	}
 }
